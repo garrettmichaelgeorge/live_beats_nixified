3cec39a1d929694a494d65d52f7f5101ad5f529a
Garrett George <garrett.michael.george@gmail.com>
2023-05-04 16:26:54 -0500

    Simplify local development with Overmind (#15)
    * Add Postgres bootstrap script
    
    * Simplify flake app declarations
    
    * Configure Overmind with Procfile in dev shell
    
    * Rename startdb -> startdbContainer
    
    * Bump Erlang to v25
    
    * Add initdb script
    
    * Add user message with instructions to dev shell

--- flake.nix
+++ flake.nix
@@ -32,64 +32,119 @@
               name = imageName;
               mixReleaseLinux = packages.mixReleaseLinux;
             };
-
-            # Start Postgres in a container
-            startdb = pkgs.writeShellApplication {
-              name = "startdb";
-              runtimeInputs = with pkgs; [ docker ];
-              text = ''
-                set -euxo pipefail
-                docker run \
-                  --name live-beats-db \
-                  --publish 5432:5432 \
-                  --env-file .env \
-                  --rm \
-                  -v pgdata:/var/lib/postgresql/data \
-                  --network ${dockerNetworkName} \
-                  postgres
-              '';
-            };
-
-            # Build image and load it into Docker
-            buildImage = pkgs.writeShellApplication {
-              name = "build-image";
-              runtimeInputs = with pkgs; [ nix docker gzip ];
-              text = ''
-                set -euxo pipefail
-                nix build .#image
-                zcat result | docker load
-              '';
-            };
-
-            # Run the container on a user-defined Docker bridge network
-            container = pkgs.writeShellApplication {
-              name = "container";
-              runtimeInputs = with pkgs; [ docker ];
-              text = ''
-                set -euxo pipefail
-                docker run \
-                  --rm \
-                  --name ${imageName} \
-                  --network ${dockerNetworkName} \
-                  --env-file .env \
-                  --pull never \
-                  --tty \
-                  --interactive \
-                  --publish 4000:4000 \
-                  ${imageName}
-              '';
-            };
-
-            connectContainer = pkgs.writeShellApplication {
-              name = "connect-to-container";
-              runtimeInputs = with pkgs; [ docker ];
-              text = ''
-                set -euxo pipefail
-                containerId="$(docker ps | grep '${imageName} ' | cut -d' ' -f 1)"
-                docker exec -it "$containerId" bash
-              '';
-            };
           };
+
+          apps =
+            let
+              # Build image and load it into Docker
+              loadImage = pkgs.writeShellApplication {
+                name = "load-image";
+                runtimeInputs = with pkgs; [ nix docker gzip packages.image ];
+                text = ''
+                  set -euxo pipefail
+                  zcat ${packages.image} | docker load
+                '';
+              };
+
+              # Run the container on a user-defined Docker bridge network
+              runContainer = pkgs.writeShellApplication {
+                name = "run-container";
+                runtimeInputs = with pkgs; [ docker loadImage ];
+                text = ''
+                  set -euxo pipefail
+
+                  load-image
+
+                  docker run \
+                    --rm \
+                    --name ${imageName} \
+                    --network ${dockerNetworkName} \
+                    --env-file .env \
+                    --pull never \
+                    --tty \
+                    --interactive \
+                    --publish 4000:4000 \
+                    ${imageName}
+                '';
+              };
+
+              # Connect to the locally running app container
+              connectContainer = pkgs.writeShellApplication {
+                name = "connect-container";
+                runtimeInputs = with pkgs; [ docker ];
+                text = ''
+                  set -euxo pipefail
+                  containerId="$(docker ps | grep '${imageName} ' | cut -d' ' -f 1)"
+                  docker exec -it "$containerId" bash
+                '';
+              };
+
+              # Initialize local Postgres cluster for development
+              # Needed before running `overmind start`
+              initdb = pkgs.writeShellApplication {
+                name = "initdb";
+                runtimeInputs = with pkgs; [ postgresql ];
+                text = ''
+                  set -eux
+                  PGDATA="./pgdata"
+                  POSTGRES_USER="postgres"
+
+                  stopDb() {
+                    pg_ctl stop
+                  }
+                  trap stopDb EXIT
+
+                  if [[ ! -d "$PGDATA" ]]; then
+                    initdb "$PGDATA"
+                  fi
+
+                  pg_ctl start
+
+                  createuser --createdb --superuser "$POSTGRES_USER" || \
+                    echo "database user $POSTGRES_USER already exists, skipping"
+
+                  createdb "$PGDATA" || \
+                      echo "database $PGDATA already exists, skipping"
+                '';
+              };
+
+              # Bootstrap local database; like `mix ecto.create` without Mix
+              bootstrapdb = pkgs.writeShellApplication {
+                name = "bootstrapdb";
+                runtimeInputs = with pkgs; [ postgresql ];
+                text = builtins.readFile ./bootstrapdb.sh;
+              };
+
+              # Start Postgres in a container
+              startdbContainer = pkgs.writeShellApplication {
+                name = "startdb-container";
+                runtimeInputs = with pkgs; [ docker ];
+                # FIXME: need to mount bootstrapdb script in a reproducible way 
+                text = ''
+                  set -euxo pipefail
+
+                  docker run \
+                    --name live-beats-db \
+                    --publish 5432:5432 \
+                    --env-file .env \
+                    --rm \
+                    --volume pgdata:/var/lib/postgresql/data \
+                    --volume bootstrapdb.sh:/docker-entrypoint-initdb.d/bootstrapdb.sh \
+                    --network ${dockerNetworkName} \
+                    postgres
+                '';
+              };
+
+              mkApp = program: { inherit program; type = "app"; };
+            in
+            {
+              loadImage = mkApp "${loadImage}/bin/load-image";
+              runContainer = mkApp "${runContainer}/bin/run-container";
+              connectContainer = mkApp "${connectContainer}/bin/connect-container";
+              bootstrapdb = mkApp "${bootstrapdb}/bin/bootstrapdb";
+              initdb = mkApp "${initdb}/bin/initdb";
+              startdbContainer = mkApp "${startdbContainer}/bin/startdb-container";
+            };
 
           devShells.default = import ./pkgs/dev-shell {
             inherit pkgs beamPackages hex elixir mixRelease;
--- pkgs/dev-shell/default.nix
+++ pkgs/dev-shell/default.nix
@@ -7,6 +7,14 @@ let
       CoreFoundation
       CoreServices
     ]);
+
+  myPostgres = pkgs.postgresql_14;
+
+  # Overmind Procfile
+  # https://github.com/DarthSim/overmind#overmind-environment
+  overmindProcfile = pkgs.writeText "Procfile" ''
+    db: ${myPostgres}/bin/postgres -k /tmp
+  '';
 in
 pkgs.mkShell {
   name = "live-beats-shell";
@@ -14,17 +22,18 @@ pkgs.mkShell {
   inputsFrom = [ mixRelease ];
 
   buildInputs = [
-    elixir
-    beamPackages.hex
     beamPackages.elixir_ls
+    beamPackages.hex
     beamPackages.rebar3
+    elixir
+    myPostgres
+    pkgs.docker
+    pkgs.gzip
     pkgs.mix2nix
     pkgs.nixpkgs-fmt
     pkgs.nixpkgs-lint
+    pkgs.overmind
     pkgs.rnix-lsp
-    pkgs.docker
-    pkgs.postgresql
-    pkgs.gzip
   ] ++ platformSpecificInputs;
 
   shellHook = ''
@@ -38,6 +47,8 @@ pkgs.mkShell {
     # Postgres
     export DATABASE_URL=ecto://postgres:postgres@localhost:5432/${database_name}
     export POOL_SIZE=15
+    export PGDATA="$PWD/pgdata"
+    export PG_LOGFILE="$PGDATA/server.log"
 
     # Scope Mix and Hex to the project directory
     mkdir -p .nix-mix
@@ -47,5 +58,37 @@ pkgs.mkShell {
     export HEX_HOME="$PWD/.nix-hex"
     export PATH="$MIX_HOME/bin:$PATH"
     export PATH="$HEX_HOME/bin:$PATH"
+
+    # Overmind
+    export OVERMIND_PROCFILE="${overmindProcfile}"
+
+    cat <<EOF
+
+    Welcome to the LiveBeats development shell! 
+
+    To initialize the database:
+
+        nix run .#initdb
+
+    To start local services in the background, including the database:
+
+        overmind start -D
+
+    To stop local services:
+
+        overmind stop
+
+    To fetch Mix deps:
+
+        mix deps.get
+
+    To start the app in development:
+        
+        iex -S mix phx.server
+
+    Make sure to set LIVE_BEATS_GITHUB_CLIENT_ID and
+    LIVE_BEATS_GITHUB_CLIENT_SECRET before compiling the app in MIX_ENV=prod.
+
+    EOF
   '';
 }
